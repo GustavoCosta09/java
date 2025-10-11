1d781d61b5505be16d38dec66f08ed841d3765e8
Gustavo <eslania.oliveira40@gmail.com>
2025-10-11 09:22:37 -0300

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,5 +1,23 @@
 import java.util.Scanner;
 public class Calculadora {
   public static void main(String[]args ) {
+    Scanner sc = new Scanner(system.in);
+    
     Double num1,num2, result;
     char operacao;
+
+    system.out.print("digite o 1º numero: ");
+    num1 = sc.nextDouble();
+    system.out.print("digite o 2º numero: ");
+    num2 = sc.nextDouble();
+    system.out.print("escolha a operacao: +, -, *, /, % ");
+    operacao = sc.nextChar();
+
+    system.ouut.print(" resultado da operacao: ");
+
+    sc.close();
+  }
+}
+
+    
+    
